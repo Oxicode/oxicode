673e97c54fe2c9622bdcd7388d598609d347e711
Oxicode <juan.quispe@upc.pe>
2021-08-23 19:47:09 -0500

    page 404

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,10 @@ import Document, { Head, Html, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
   render () {
+    const randomColorClass = (Math.random() > 0.5) ? 'bg-gradient-to-l' : 'bg-gradient-to-bl'
+
     return (
-      <Html>
+      <Html className= {`${randomColorClass} from-gray-700 via-gray-900 to-black`}>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
 module.exports = {
   mode: 'jit',
-  purge: [
-    './pages/*.js',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
-  ],
+  purge: {
+    content: [
+      './pages/*.js',
+      './pages/**/*.{js,ts,jsx,tsx}',
+      './components/**/*.{js,ts,jsx,tsx}'
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {}
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
