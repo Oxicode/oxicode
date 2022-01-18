1879cdf841228c65eba6d5de91425203d37aed1c
Oxicode <christian.quispeh@gmail.com>
2022-01-18 14:29:10 -0500

    done

---
+++ components/helper.js
@@ -0,0 +1,5 @@
+const classNames = (...classes) => classes.filter(Boolean).join(' ')
+
+export {
+  classNames
+}
--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,14 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
 
+import { classNames } from '@/components/helper'
+
 class MyDocument extends Document {
   render () {
     const randomColorClass = (Math.random() > 0.5) ? 'bg-gradient-to-l' : 'bg-gradient-to-bl'
     const randomBGClass = (Math.random() > 0.5) ? 'bg-[url("/svg/circuit-board.svg")]' : ''
 
     return (
-      <Html className={`${randomColorClass} from-gray-700 via-gray-900 to-black`} lang='en'>
+      <Html className={classNames(randomColorClass, 'from-gray-700 via-gray-900 to-black')} lang='en'>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
@@ -17,7 +19,7 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
         </Head>
-        <body className={`overflow-hidden ${randomBGClass}`}>
+        <body className={classNames('overflow-hidden', randomBGClass)}>
           <Main />
           <NextScript />
           {randomBGClass === ''
